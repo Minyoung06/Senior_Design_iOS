c21b459a8f7e69c05bb1fb2cbb0df2f2b6090cfe
Minyoung06 <monica_y@naver.com>
2022-04-29 23:39:02 -0400

    Initial Commit

---
+++ Senior_Design/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Senior_Design
+//
+//  Created by MinYoung Yang on 4/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
