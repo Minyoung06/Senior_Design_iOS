32ab016e93f7eb55adf53a792b5ec0adb0dd42c9
Minyoung06 <monica_y@naver.com>
2022-05-03 18:50:19 -0400

    Connect UIs Import MK, CL to MapViewController Add localManager things at MapViewController to get current location Add location authentication at Info.plist

--- Senior_Design/MapViewController.swift
+++ Senior_Design/MapViewController.swift
@@ -6,17 +6,25 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
     
-    
+    @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest   //accuracy
+        locationManager.requestWhenInUseAuthorization() //permission
+        locationManager.startUpdatingLocation()
+        mapView.showsUserLocation = true
+        
+        
     }
-    
-
 }
+
