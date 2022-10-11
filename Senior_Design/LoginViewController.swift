9462edcd8ce66328730b2c6fd035af7061d145ec
Cynthia <czhao9@pride.hofstra.edu>
2022-10-11 14:23:00 -0400

    Added Sign out button and capabilities in the user profile account page

--- Senior_Design/LoginViewController.swift
+++ Senior_Design/LoginViewController.swift
@@ -22,29 +22,35 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func loginTapped(_ sender: Any) {
         
-        //get cleaned versions of the text fields
-        let email = usernameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        //make the tab bar controller the main view controller
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
         
-        //sign in the user
-        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            
-            if error != nil {
-                //failed to sign in
-                self.errorLabel.isHidden = false
-                self.errorLabel.text = error!.localizedDescription
-                self.errorLabel.alpha = 1
-            }
-            else {
-                //make the tab bar controller the main view controller
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
-                
-                (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
-                
-                //self.performSegue(withIdentifier: "loginSegue", sender: self)
-            }
-        }
+//        //get cleaned versions of the text fields
+//        let email = usernameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+//
+//        //sign in the user
+//        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+//
+//            if error != nil {
+//                //failed to sign in
+//                self.errorLabel.isHidden = false
+//                self.errorLabel.text = error!.localizedDescription
+//                self.errorLabel.alpha = 1
+//            }
+//            else {
+//                //make the tab bar controller the main view controller
+//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//                let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
+//
+//                (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(mainTabBarController)
+//
+//                //self.performSegue(withIdentifier: "loginSegue", sender: self)
+//            }
+//        }
     }
     
     @IBOutlet weak var errorLabel: UILabel!
